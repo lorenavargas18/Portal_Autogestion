be15cdb7689a9d7dfcc2e699afe4e289c93b3771
camilo004 <83430070+camilo004@users.noreply.github.com>
2023-10-06 19:03:56 -0500

    api
    api

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+
 
 // Add DbContext
 builder.Services.AddDbContext<MyDbContext>(options =>
